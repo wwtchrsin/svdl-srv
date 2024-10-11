a970f94dbeeb08b6a10d456b7f8fcf2c06237221
Vladislav Chursin <wwtchrsin@yandex.ru>
2024-10-12 06:37:23 +0700

    load script changed

--- scripts/onload.sh
+++ scripts/onload.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
+if [ ! -d "./public/videos" ]; then
+  mkdir ./public
+  mkdir ./public/videos
+fi
+
+if [ ! -f "./yt-dlp/yt-dlp" ]; then
+  mkdir ./yt-dlp
+  wget https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp_linux -O ./yt-dlp/yt-dlp
+  chmod ug+x ./yt-dlp/yt-dlp
+fi
+
 rm -rf ./public/videos/*
